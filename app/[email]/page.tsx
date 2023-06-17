a6f682e42815541fd308bcdf5336e4ba5eda4e2b
Stepan Vetrovec <s.vetrovec@cleo.one>
2023-06-17 23:13:44 +0200

    Initial commit

---
+++ app/[email]/page.tsx
@@ -0,0 +1,10 @@
+export const runtime = 'edge';
+
+export default function Email() {
+	return (
+		<main className="h-full flex flex-col justify-center items-center gap-4">
+			<h1 className="text-2xl">Waitlist</h1>
+			<p>You were signed up</p>
+		</main>
+	);
+}
